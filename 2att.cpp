9ed524ff8c581f53ef098d325cc8e37d1b49339a
Yash45679 <92053120+Yash45679@users.noreply.github.com>
2021-10-06 23:52:05 +0530

    Add files via upload

---
+++ 2att.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+ 
+
+int main(){
+    int arr[5];
+    for(int i=0;i<5;i++){
+        cin>>arr[i];
+    }
+    
+    if(abs(arr[0]-arr[1])==0){
+        
+    }
+   
+    
+    cout<<arr[0];
+    
+	// your code goes here
+	return 0;
+}
